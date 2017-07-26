a7dce766fe954f203a2fb1fe15bca12b17b84cb2
csq-dr <chuchuwei@umass.edu>
2017-07-26 19:54:41 -0400

    updateR

--- update-r-packages.R
+++ update-r-packages.R
@@ -1,3 +1,7 @@
 oldpkgs = list.files("/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
 current = list.files("/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 install.packages(setdiff(oldpkgs, current))
+
+oldpkgs.win = list.files("C:/Program Files/R/R-3.3.2/library")
+current.win = list.files("C:/Program Files/R/R-3.4.1/library")
+setdiff(oldpkgs.win, current.win)
